8d2d419b427c81d8262e9af39bd5bd475375f31f
ytx1991 <ytx1991@gmail.com>
2021-06-07 15:54:35 -0700

    fix grafana permission issue

--- server/install_server.sh
+++ server/install_server.sh
@@ -3,10 +3,11 @@
 mkdir ~/swarmon_server
 sudo mkdir /root/swarmon/prometheus
 sudo mkdir /root/swarmon/grafana
-sudo chmod -R 777 /root/swarmon/grafana
+
 cd ~/swarmon_server
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/server/grafana/grafana.db
 sudo cp grafana.db /root/swarmon/grafana/grafana.db
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/server/docker-compose.yaml
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/server/prometheus.yml
 sudo cp prometheus.yml /root/swarmon/prometheus
+sudo chmod -R 777 /root/swarmon/grafana
