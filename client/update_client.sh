bee31084f2982bd41cf0733d6b493b99985bd281
ytx1991 <ytx1991@gmail.com>
2021-06-07 20:04:32 -0700

    change update script

--- client/update_client.sh
+++ client/update_client.sh
@@ -5,11 +5,9 @@ cd ~/swarmon_client
 cd progs
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/client/swarm.mtail -O swarm.mtail
 cd ..
-wget https://github.com/ytx1991/SwarmMonitoring/raw/main/client/run_datacollector.sh -O run_datacollector.sh
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/client/stop_datacollector.sh -O stop_datacollector.sh
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/client/get_metrics.sh -O get_metrics.sh
 wget https://github.com/ytx1991/SwarmMonitoring/raw/main/client/update_client.sh -O update_client.sh
-chmod +x stop_datacollector.sh
 chmod +x run_datacollector.sh
 chmod +x get_metrics.sh
 chmod +x update_client.sh
